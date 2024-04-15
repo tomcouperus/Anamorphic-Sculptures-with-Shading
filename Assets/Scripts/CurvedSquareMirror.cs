1f39525653c521fd1d5dacc5e8d4b0221b28aab0
tom couperus <tcouperus@hotmail.nl>
2024-04-15 22:31:55 +0200

    Make mirror procedurally curved

--- Assets/Scripts/CurvedSquareMirror.cs
+++ Assets/Scripts/CurvedSquareMirror.cs
@@ -10,6 +10,14 @@ public class CurvedSquareMirror : ProceduralMirror {
     [Range(3, 256)]
     private int ySize = 3;
 
+    private enum CurveDirection { Horizontal, Vertical };
+    [Header("Curvature")]
+    [SerializeField]
+    private CurveDirection curveDirection = CurveDirection.Horizontal;
+    [SerializeField]
+    [Min(0)]
+    private float radiusOfCurvature = 1;
+
     protected override MeshData GenerateMeshData() {
         MeshData data = new(xSize, ySize);
         int vertexIndex = 0;
@@ -18,8 +26,23 @@ public class CurvedSquareMirror : ProceduralMirror {
                 // For now, generate a plane
                 float uCoord = (float) x / (xSize - 1);
                 float vCoord = (float) y / (ySize - 1);
-                float zCoord = 0;
-                Vector3 vertex = new(uCoord * 2 - 1, vCoord * 2 - 1, zCoord);
+                float xCoord = uCoord * 2 - 1;
+                float yCoord = vCoord * 2 - 1;
+                float zCoord;
+
+                if (curveDirection == CurveDirection.Horizontal) {
+                    float distOpticalCentre = radiusOfCurvature * Mathf.Cos(Mathf.Asin(1 / radiusOfCurvature));
+
+                    float alpha = Mathf.Asin(yCoord / radiusOfCurvature);
+                    zCoord = radiusOfCurvature * Mathf.Cos(alpha) - distOpticalCentre;
+                } else {
+                    float distOpticalCentre = radiusOfCurvature * Mathf.Sin(Mathf.Acos(1 / radiusOfCurvature));
+
+                    float alpha = Mathf.Acos(xCoord / radiusOfCurvature);
+                    zCoord = radiusOfCurvature * Mathf.Sin(alpha) - distOpticalCentre;
+                }
+
+                Vector3 vertex = new(xCoord, yCoord, zCoord);
                 Vector2 uv = new(uCoord, vCoord);
 
                 data.AddVertex(vertex, uv, vertexIndex);
