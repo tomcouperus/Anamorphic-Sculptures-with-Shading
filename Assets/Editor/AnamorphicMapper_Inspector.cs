c7547cecfaaa56967d5bf7f827d8d559e580e153
tom couperus <tcouperus@hotmail.nl>
2024-04-17 22:03:42 +0200

    Get raycast intersection

--- Assets/Editor/AnamorphicMapper_Inspector.cs
+++ Assets/Editor/AnamorphicMapper_Inspector.cs
@@ -3,16 +3,18 @@ using UnityEditor;
 
 [CustomEditor(typeof(AnamorphicMapper))]
 public class AnamorphicMapper_Inspector : Editor {
+
     public override void OnInspectorGUI() {
         base.OnInspectorGUI();
         AnamorphicMapper anamorphicMapper = (AnamorphicMapper) target;
+
+        EditorGUILayout.MinMaxSlider(ref anamorphicMapper.showMin, ref anamorphicMapper.showMax, anamorphicMapper.ShowMinLimit, anamorphicMapper.ShowMaxLimit);
 
         if (GUILayout.Button("Map")) {
             anamorphicMapper.MapObject();
         }
         if (GUILayout.Button("Destroy")) {
-            anamorphicMapper.globalMeshVertices = null;
-            anamorphicMapper.raycastDirections = null;
+            anamorphicMapper.Clear();
         }
     }
 }
--- Assets/Scripts/AnamorphicMapper.cs
+++ Assets/Scripts/AnamorphicMapper.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class AnamorphicMapper : MonoBehaviour {
+    [Header("Settings")]
     [SerializeField]
     private Transform viewer;
     [SerializeField]
@@ -11,8 +11,25 @@ public class AnamorphicMapper : MonoBehaviour {
     [SerializeField]
     private GameObject anamorphObject;
 
-    public Vector3[] globalMeshVertices;
-    public Vector3[] raycastDirections;
+    [Header("Debug")]
+    [SerializeField]
+    private Vector3[] globalMeshVertices;
+    [SerializeField]
+    private Vector3[] raycastDirections;
+    [SerializeField]
+    private Vector3[] raycastHits;
+
+    [SerializeField]
+    private bool showGlobalMeshVertices = false;
+    [SerializeField]
+    private bool showRaycastDirections = false;
+    [SerializeField]
+    private bool showRaycastHits = false;
+
+    public float showMin = 0;
+    public float showMax = 0;
+    public float ShowMinLimit { get; private set; } = 0;
+    public float ShowMaxLimit { get; private set; } = 0;
 
     public void MapObject() {
         // Transform all vertices of target mesh to global space.
@@ -30,17 +47,42 @@ public class AnamorphicMapper : MonoBehaviour {
         for (int i = 0; i < vertices.Length; i++) {
             raycastDirections[i] = globalMeshVertices[i] - origin;
         }
+        // Do the raycasting
+        raycastHits = new Vector3[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++) {
+            RaycastHit[] hits = Physics.RaycastAll(origin, raycastDirections[i].normalized, 20f);
+            if (hits.Length > 0) {
+                raycastHits[i] = hits[0].point;
+            } else {
+                Debug.Log("No hit with mirror");
+            }
+        }
+        ShowMaxLimit = vertices.Length - 1;
+    }
+
+    public void Clear() {
+        globalMeshVertices = null;
+        raycastDirections = null;
+        raycastHits = null;
     }
 
     private void OnDrawGizmosSelected() {
-        if (globalMeshVertices != null) {
-            foreach (Vector3 v in globalMeshVertices) {
-                Gizmos.DrawSphere(v, 0.1f);
+        Gizmos.color = Color.gray;
+        if (showGlobalMeshVertices && globalMeshVertices != null) {
+            for (int i = (int) showMin; i <= showMax; i++) {
+                Gizmos.DrawSphere(globalMeshVertices[i], 0.1f);
             }
         }
-        if (raycastDirections != null && viewer != null) {
-            foreach (Vector3 d in raycastDirections) {
-                Gizmos.DrawLine(viewer.position, viewer.position + d);
+        Gizmos.color = Color.blue;
+        if (showRaycastDirections && raycastDirections != null && viewer != null) {
+            for (int i = (int) showMin; i <= showMax; i++) {
+                Gizmos.DrawLine(viewer.position, viewer.position + raycastDirections[i]);
+            }
+        }
+        Gizmos.color = Color.white;
+        if (showRaycastHits && raycastHits != null) {
+            for (int i = (int) showMin; i <= showMax; i++) {
+                Gizmos.DrawSphere(raycastHits[i], 0.1f);
             }
         }
     }
