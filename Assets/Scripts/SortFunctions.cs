7cbffbda5ef35049de92d3aedd1e9131c14d8fe2
tom couperus <tcouperus@hotmail.nl>
2024-05-26 15:20:53 +0200

    More sorting functions

--- Assets/Scripts/SortFunctions.cs
+++ Assets/Scripts/SortFunctions.cs
@@ -1,6 +1,24 @@
 using System.Collections.Generic;
 
 public static class SortFunctions {
+
+    // KeyValuePair KEY SORTERS
+    public static int smallToLargeKeySorter(KeyValuePair<int, float> pair1, KeyValuePair<int, float> pair2) {
+        return pair1.Key.CompareTo(pair2.Key);
+    }
+
+    public static int largeToSmallKeySorter(KeyValuePair<int, float> pair1, KeyValuePair<int, float> pair2) {
+        return pair2.Key.CompareTo(pair1.Key);
+    }
+    public static int smallToLargeKeySorter(KeyValuePair<float, float> pair1, KeyValuePair<float, float> pair2) {
+        return pair1.Key.CompareTo(pair2.Key);
+    }
+
+    public static int largeToSmallKeySorter(KeyValuePair<float, float> pair1, KeyValuePair<float, float> pair2) {
+        return pair2.Key.CompareTo(pair1.Key);
+    }
+
+    // KeyValuePair VALUE SORTERS
     public static int smallToLargeValueSorter(KeyValuePair<int, float> pair1, KeyValuePair<int, float> pair2) {
         return pair1.Value.CompareTo(pair2.Value);
     }
